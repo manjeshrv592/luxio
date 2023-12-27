fceb56719ca8ddec7c832d5c15aa43f1ad755b93
Manjesh R V <manjeshrv592@gmail.com>
2023-12-27 11:18:11 +0530

    Added luxio orange and grey colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,32 @@ module.exports = {
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
       },
+      colors: {
+        'luxio-orange': {
+          50: '#FFF2EC',
+          100: '#FFE5D9',
+          200: '#FFCBB3',
+          300: '#FFB28C',
+          400: '#FF9866',
+          500: '#FF7E40',
+          600: '#CC6533',
+          700: '#994C26',
+          800: '#66321A',
+          900: '#33190D',
+        },
+        'luxio-grey': {
+          50: '#F2F2F2',
+          100: '#E6E6E6',
+          200: '#CCCCCC',
+          300: '#B3B3B3',
+          400: '#999999',
+          500: '#808080',
+          600: '#666666',
+          700: '#4D4D4D',
+          800: '#333333',
+          900: '#1A1A1A',
+        },
+      },
       keyframes: {
         'accordion-down': {
           from: { height: 0 },
